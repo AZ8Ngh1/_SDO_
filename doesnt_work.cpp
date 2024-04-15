788ba781fd13079243c87163ec27b34ba335c6d7
Az8n <146655473+AZ8Ngh1@users.noreply.github.com>
2024-04-15 20:38:59 +0300

    Add files via upload

---
+++ doesnt_work.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+int main()
+{
+
+
+    string a; string b; cin >> a >> b;
+    
+    string l_three_a = a.substr(a.size() - 3);
+    string l_three_b = a.substr(a.size() - 3);
+
+    
+    int l_three_1 = stoi(l_three_b, 0);
+    int l_three_2 = stoi(l_three_a, 0);
+    
+    int sum = l_three_1 + l_three_2;
+    cout << sum;
+    
+    return 0;
+}
+
+
+
